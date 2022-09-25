a2d9186509e134d7dc7b67079f6b4249275b36fd
ronald <ronaldcoello85@gmail.com>
2022-09-24 18:06:13 -0600

    finish assignment 2

--- lesson2/js/script.js
+++ lesson2/js/script.js
@@ -2,3 +2,5 @@ let date = new Date()
 
 let lastUpdate = document.lastModified;
 document.querySelector('.update').innerHTML = `Last Updated ${lastUpdate}`
+
+document.querySelector('.copyright').innerHTML = `© ${date.getFullYear()} | Ronald Coello |`
