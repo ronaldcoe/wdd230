68d7d100c1991414f13cf8a984644ab95857e1bc
ronald <ronaldcoello85@gmail.com>
2022-10-13 12:41:44 -0600

    dom practice

---
+++ lesson5/js/scripts.js
@@ -0,0 +1,23 @@
+
+let button = document.querySelector(".add")
+
+button.addEventListener("click", ()=> {
+    let chapter = document.querySelector("#chapter").value;
+
+    let list = document.querySelector(".list")
+
+    let li = document.createElement("li");
+    let del = document.createElement("button");
+    del.className = "delete";
+    del.textContent = "X"
+    li.appendChild(document.createTextNode(chapter));
+    document.querySelector("#chapter").value = ""
+    li.appendChild(del);
+    
+    list.appendChild(li)
+    
+    del.addEventListener("click", ()=> {
+        list.removeChild(li)
+    })
+
+})
