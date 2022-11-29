ba23b41437676f4364e24d5c95a848ac2ddbb458
ronald <ronaldcoello85@gmail.com>
2022-11-29 13:09:11 -0700

    site plan

--- final_project/script/characters.js
+++ final_project/script/characters.js
@@ -11,11 +11,17 @@ function displayCharacters(characters) {
     let h3 = document.createElement("h3");
     let img = document.createElement("img")
 
+    div.addEventListener("click", displayName(characters))
     h3.textContent = characters.name
     img.src = characters.icon
     div.appendChild(img)
     div.appendChild(h3)    
     container.appendChild(div) 
+
+}
+
+function displayName(characters) {
+    console.log(characters.name)
 }
 async function getCharacters(url) {
     const response = await fetch(url);
