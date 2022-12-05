259718b85f3ac1d0b8a80292b56209eb6e3826f0
ronald <ronaldcoello85@gmail.com>
2022-12-04 18:44:52 -0700

    art

---
+++ final_project/script/quotes.js
@@ -0,0 +1,45 @@
+const url = "./script/quotes.json";
+
+function displayQuotes(quotes) {
+
+    let container = document.querySelector(".quotesContainer")
+
+    let div = document.createElement("div")
+    let divText = document.createElement("div")
+    let h2 = document.createElement("h2")
+    let p = document.createElement("p")
+    let author = document.createElement("p")
+    let img = document.createElement("img")
+    p.textContent = quotes.quote
+    img.src = quotes.image
+    author.textContent = `- ${quotes.author}`
+    author.style.textAlign = "left"
+    author.style.fontSize = "18px"
+    divText.appendChild(p)
+    divText.appendChild(author)
+   
+    div.appendChild(img)
+    div.appendChild(divText);
+
+
+    container.appendChild(div)
+
+}
+
+async function getQuotes(url) {
+    const response = await fetch(url)
+
+    if(response.ok) {
+        const jsObject = await response.json()
+        let quotes = jsObject["quotes"]
+        console.log(quotes)
+        quotes.forEach(displayQuotes);
+    }
+
+    
+
+}
+
+
+
+getQuotes(url)
