543a221a4b3b864bfbe3e04723e9088daa970fdb
ronald <ronaldcoello85@gmail.com>
2022-12-08 12:06:18 -0700

    characters

--- final_project/script/characters.js
+++ final_project/script/characters.js
@@ -52,3 +52,72 @@ async function getCharacters(url) {
 
 
 getCharacters(charactersURL)
+
+
+
+
+
+
+// Character carousel 
+
+let left = document.querySelector(".fa-circle-arrow-left")
+let right = document.querySelector(".fa-circle-arrow-right")
+
+
+let carousel = document.querySelector(".characterContainer")
+
+
+function moveLeft() {
+    // let carousel = document.querySelector(".characterContainer")
+    carousel.scrollBy({top:0, left:-400, behavior:"smooth"})
+    console.log("left")
+}
+
+function moveRight() {
+    // let carousel = document.querySelector(".characterContainer")
+    carousel.scrollBy({top:0, left:400, behavior:"smooth"})
+}
+
+
+
+left.addEventListener("click",  moveLeft)
+right.addEventListener("click", moveRight)
+
+
+
+
+let pos = {left:0, x:0} 
+
+
+const mouseMoveHandler = function (e) {
+    // How far the mouse has been moved
+    const dx = e.clientX - pos.x;
+
+    // Scroll the element
+    carousel.style.cursor = 'grabbing';
+    carousel.style.userSelect = 'none';
+    carousel.scrollLeft = pos.left - dx;
+};
+const mouseUpHandler = function () {
+    document.removeEventListener('mousemove', mouseMoveHandler);
+    document.removeEventListener('mouseup', mouseUpHandler);
+
+    carousel.style.cursor = 'grab';
+    carousel.style.removeProperty('user-select');
+};
+
+
+const mouseDownHandler = function (e){
+    carousel.style.cursor = 'grabbing';
+    carousel.style.userSelect = 'none';
+    pos = {
+        left: carousel.scrollLeft,
+        x: e.clientX
+    }
+
+    document.addEventListener("mousemove", mouseMoveHandler)
+    document.addEventListener("mouseup", mouseUpHandler)
+}
+
+carousel.addEventListener("mousedown", mouseDownHandler)
+// carousel.removeEventListener("mouseup", mouseUpHandler)
